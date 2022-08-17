0bcf140ffec6c3929cc7557e35236837af97f608
FedorovMG <FedorovMG@hotmail.com>
2022-08-17 15:23:55 +0500

    Add DAG

--- FinalProject/Airflow/dags/FirstDag.py
+++ FinalProject/Airflow/dags/FirstDag.py
@@ -1,8 +1,30 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import PythonOperator
 from clickhouse_driver import Client
 
-client = Client('0.0.0.0')
-rezult = client.execute("SHOW DATABASES")
-print(rezult)
+default_args={
+    'owner':'FedorovMG'
+}
+
+def showDatabase():
+    client = Client('clickhouse')
+    rezult = client.execute('SHOW DATABASES')
+    for r in rezult:
+        print(r)
+
+
+with DAG(
+    default_args=default_args,
+    dag_id='show_databases',
+    description='show databases',
+    start_date=datetime(2022,8,14),
+    schedule_interval='@daily'
+)as dag:
+    task1 = PythonOperator(
+        task_id='showDatabases',
+        python_callable=showDatabase
+    )
+
+    task1
+    
