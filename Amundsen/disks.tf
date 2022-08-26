166c7c4af59db4da23102bdfd0d944bda6f2bec8
FedorovMG <FedorovMG@hotmail.com>
2022-08-26 13:57:06 +0500

    Edir Redash

---
+++ Amundsen/disks.tf
@@ -0,0 +1,5 @@
+resource "yandex_compute_disk" "disk_amundsen" {
+  type = "network-hdd"
+  size = "20"
+  image_id = "fd8cqj9qiedndmmi3vq6"
+}
---
+++ Amundsen/network.tf
@@ -0,0 +1,10 @@
+resource "yandex_vpc_network" "data_net" {
+  name = "data_net"
+}
+
+resource "yandex_vpc_subnet" "data_subnet" {
+  name = "MySubnet"
+  zone = var.zone
+  network_id = yandex_vpc_network.data_net.id
+  v4_cidr_blocks = [ "10.0.1.0/24" ]
+}
